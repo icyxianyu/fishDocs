be65c9f3d440f080f6ce3e5b846416986fb42b8f
icyxianyu <3345625156@qq.com>
2023-12-19 19:03:13 +0800

    feat: 实现ssg

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,6 +7,10 @@ export const SRCPATH = path.join(ROOTROAD, 'src')
 
 export const DISTPATH = path.join(ROOTROAD, 'dist');
 
+export const BUILDPATH = path.join(ROOTROAD, 'build');
+
+export const BUILDTEMPPATH = path.join(BUILDPATH, '.temp');
+
 export const TEMPLATEPATH = path.join(ROOTROAD, 'template.html');
 
 export const CLIENTENTRY = path.join(SRCPATH, 'runtime', 'client-entry.tsx');
--- src/node/build.ts
+++ src/node/build.ts
@@ -1,5 +1,7 @@
 import { build as viteBuild } from 'vite';
-import { CLIENTENTRY, SERVERENTRY } from '../constants';
+import { BUILDPATH, BUILDTEMPPATH, CLIENTENTRY, SERVERENTRY } from '../constants';
+import { join } from 'path';
+import * as fs from "fs-extra";
 
 export async function bundle(root: string) {
     try {
@@ -10,7 +12,7 @@ export async function bundle(root: string) {
                 root,
                 mode: 'production',
                 build: {
-                    outDir: 'build/client',
+                    outDir: join(BUILDTEMPPATH,'client'),
                     rollupOptions: {
                         input: CLIENTENTRY,
                         output: {
@@ -28,7 +30,7 @@ export async function bundle(root: string) {
                 mode: 'production',
                 build: {
                     ssr: true,
-                    outDir: 'build/server',
+                    outDir:join(BUILDTEMPPATH,'server'),
                     rollupOptions: {
                         input: SERVERENTRY,
                         output: {
@@ -38,19 +40,58 @@ export async function bundle(root: string) {
                 }
             })
         }
+       const [clientBundle,serverBundle] = await Promise.all([clientResult(), serverResult()]);
 
-        await clientResult();
-        await serverResult();
+       return [clientBundle,serverBundle];
 
     } catch (e) {
         throw new Error(e);
     }
 }
 
+export const renderPage = async (render: ()=>string,root: string,clientBundle: any) => {
+
+    const clientChunk = clientBundle.output.find(
+        (chunk) => chunk.type === "chunk" && chunk.isEntry
+      );
+    // 获取客户端打包后的文件名
+      console.log("Rendering page in server side...");
+     const appHTML = render();
+     // 获取server端render出来的HTML；
+     const html = `
+     <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+            <title>title</title>
+            <meta name="description" content="xxx">
+        </head>
+        <body>
+            <div id="root">${appHTML}</div>
+            <script type="module" src="./${clientChunk?.fileName}"></script>
+        </body>
+        </html>
+     `.trim();
+    // 模版html 
+
+    await fs.ensureDir(BUILDPATH);
+    await fs.writeFile(join(BUILDPATH,'index.html'), html);
+    await fs.copy(join(BUILDTEMPPATH,'client'), join(BUILDPATH) );
+    await fs.remove(BUILDTEMPPATH);
+};
+
 export async function build(root: string) {
     // bundle 逻辑 打包client端和server端;
     // 引入 server-entry 模块
     // 服务端渲染，产出HTML
 
-    await bundle(root);
+   const [clientBundle,serverBundle] =  await bundle(root);
+
+   const serverEntryPATH = join(BUILDTEMPPATH,'server','server-entry.js');
+
+   const { renderInNode } = require(serverEntryPATH);
+
+   await renderPage(renderInNode,root,clientBundle)
+
 }
